58464248231871f4c03b8095382a4699fa21b8e9
Kirti Vardhan <kirtivardhan8@gmail.com>
2024-03-15 20:51:17 +0530

    Project created

---
+++ src/main/java/com/service/masterdata/YobMasterdataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.masterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobMasterdataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobMasterdataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/masterdata/YobMasterdataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.masterdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YobMasterdataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
