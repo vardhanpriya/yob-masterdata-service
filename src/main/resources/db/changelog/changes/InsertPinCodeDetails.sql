8f4e99ad4598e8d3a39e5f0f5af449771e4138ee
priya vardhan <vardhanpriya001@gmail.com>
2024-07-29 23:41:41 +0530

    added pincode details

---
+++ src/main/resources/db/changelog/changes/InsertPinCodeDetails.sql
@@ -0,0 +1,5 @@
+INSERT INTO Pincode_Details(Pincode,City,State) VALUES (804408,'JEHANABAD','BIHAR');
+INSERT INTO Pincode_Details(Pincode,City,State) VALUES (804454,'DAULATPUR','BIHAR');
+INSERT INTO Pincode_Details(Pincode,City,State) VALUES (823001,'GAYA','BIHAR');
+INSERT INTO Pincode_Details(Pincode,City,State) VALUES (411000,'INDORE','MADHYA PRADESH');
+INSERT INTO Pincode_Details(Pincode,City,State) VALUES (411057,'PUNE','MAHARASHTRA');
